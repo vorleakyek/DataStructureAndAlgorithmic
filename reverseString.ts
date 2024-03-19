c505ac0c31e435ed39b15e040030e34ef70d08dd
Vorleak Yek <vorleakyek@gmail.com>
2024-03-18 22:53:03 -0700

    commonItems O(n^2)

--- reverseString.ts
+++ reverseString.ts
@@ -12,8 +12,30 @@
 };
 
 
-// Input: s = ["h","e","l","l","o"]
-// Output: ["o","l","l","e","h"]
+// function reverseString2(s: string): string {
+//     //check input 
+//     if (!s || s.length <2 || typeof s !== 'string') {
+//         return 'not a valid string'; 
+//     }
 
-// Input: s = ["H","a","n","n","a","h"]
-// Output: ["h","a","n","n","a","H"]
+//     const backwards: string[] = [];
+//     const totalChars = s.length - 1;
+
+//     for (let i = totalChars; i >= 0; i--) {
+//         backwards.push(s[i]); 
+//     }
+
+//     const result = backwards.join('');
+
+//     console.log(result);
+//     return result;
+// }
+
+// // Input: s = ["h","e","l","l","o"]
+// // Output: ["o","l","l","e","h"]
+
+// //Input: s = ["H","a","n","n","a","h"]
+// // Output: ["h","a","n","n","a","H"]
+
+
+// reverseString2('hello');
